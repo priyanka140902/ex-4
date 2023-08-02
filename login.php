13a7fdef2c71e7c3bfcac79b86e65d5ede157dd9
priyanka140902 <141222883+priyanka140902@users.noreply.github.com>
2023-08-02 15:36:36 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,33 +1,33 @@
-<?php
- $usr = $_POST["usr"];
- $pwd = $_POST["pwd"];
- if($usr == “priyanka” && $pwd == “priyanka123”)
- {
- $loginName = "Welcome Admin";
- }
- else
- {
- $loginName = "Login Failed";
- }
+<?php  
+	$usr = $_POST["usr"];
+	$pwd = $_POST["pwd"];
+	if($usr == "sajid" && $pwd == "sajid123")
+	{
+		$loginName = "Welcome Admin";
+	}
+	else
+	{
+		$loginName = "Login Failed";
+	}
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-<title>Login Page</title>
-<link rel="stylesheet" href="sitestyle.css">
-</head>
-<body>
-<header>
-<h1>Cloud Login Result</h1>
-</header>
-<hr>
-<article>
-<section>
-<h2> <?php echo $loginName ?></h1>
-</section>
-</article>
-<footer>
-Developed by K Anbarasan
-</footer>
-</body>
+	<head>
+		<title>Login Page</title>	
+		<link rel="stylesheet" href="sitestyle.css">	
+	</head>
+	<body>		
+		<header>
+			<h1>Cloud Login Result</h1>
+		</header>	
+		<hr>
+		<article>			
+			<section>
+				<h2> <?php echo $loginName ?></h1>
+			</section>
+		</article>
+		<footer>
+			Developed by K Anbarasan
+		</footer>
+	</body>
 </html>
